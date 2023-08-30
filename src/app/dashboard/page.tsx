03b75ba9f6c527ccd72ff0d2b48d700ccbb3c4bb
AniruddhUdayan <aniruddhudayan34@gmail.com>
2023-08-31 03:56:28 +0530

    Setup the project and rise database

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Button from '@/components/ui/Button';
+import React, { FC } from 'react';
+
+interface Props {
+  // Define your component's props here
+}
+
+const Dashboard: FC<Props> = (props) => {
+  return (
+   <Button>Hello</Button>
+  );
+};
+
+export default Dashboard;
